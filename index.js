975b34d664b2f899148ce526b9f53d44b89db095
Aswin Bouwmeester <aswin.bouwmeester@gmail.com>
2024-10-24 00:15:43 +0200

    added start and endtime parammeters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
   var plugin = {};
-  plugin.id = "nmea0183-log-streamer";
-  plugin.name = "NMEA0183 Log Streamer";
+  plugin.id = "nmea-streamer";
+  plugin.name = "NMEA-streamer";
   plugin.description = "The plugin streams a file with NMEA0183 messages to the Signal K server at a configurable speed.";
   plugin.author = "aswin.bouwmeester@gmail.com";
 
@@ -19,6 +19,16 @@ module.exports = function (app) {
         type: "string",
         title: "Filename",
         description: "Full path to the log file"
+      },
+      startTime: {
+        type: "string",
+        title: "Start time",
+        description: "Optional start time in HH:MM:SS format"
+      },
+      endTime: {
+        type: "string",
+        title: "End time",
+        description: "Optional end time in HH:MM:SS format"
       },
       speedFactor: {
         type: "number",
@@ -48,10 +58,6 @@ module.exports = function (app) {
   client.on('error', (error) => {
     app.error('TCP Connection Error: ', error);
   });
-
-
-
-
 
 
   plugin.start = function (options) {
@@ -90,6 +96,7 @@ module.exports = function (app) {
     const startReading = async () => {
       while (true) {
         if (stopEvent) {
+          stopEvent = false;
           break;
         }
         app.debug(`Start processing ${filename}`);
@@ -99,23 +106,33 @@ module.exports = function (app) {
 
     function handleLine(line) {
       let wait = 0;
+      if (!isValidNMEA0183(line)) {
+        //app.debug(`msg ignored: ${line}`);
+        return 0;
+      }
       msg = line.substr(3, 3);
       let rmcTime = null;
       // CGA:1, GLL:5, RMC:1, ZDA:1, 
       switch (msg) {
         case 'CGA':
-          rmcTime = getTime(line, 1);
+          rmcTime = toDate(line.split(',')[1]);
           break;
         case ('GLL'):
-          rmcTime = getTime(line, 5);
+          rmcTime = toDate(line.split(',')[5]);
           break;
         case 'RMC':
-          rmcTime = getTime(line, 1);
+          rmcTime = toDate(line.split(',')[1]);
           break;
         case 'ZDA':
-          rmcTime = getTime(line, 1);
+          rmcTime = toDate(line.split(',')[1]);
           break;
       }
+      if (rmcTime) {
+        if (startTime && startTime < rmcTime) withinTimeWindow=true;
+        if (endTime && endTime < rmcTime) withinTimeWindow=false;
+      }
+
+      if (!withinTimeWindow) return 0;
 
       if (rmcTime) {
         const now = Date.now();
@@ -129,69 +146,81 @@ module.exports = function (app) {
       return wait;
     }
 
-    function getTime(line, i) {
-      const nmeaTime = line.split(',')[i];
-      const hours = parseInt(nmeaTime.substr(0, 2), 10);
-      const minutes = parseInt(nmeaTime.substr(2, 2), 10);
-      const seconds = parseInt(nmeaTime.substr(4, 2), 10);
-      const miliseconds = parseInt(nmeaTime.substr(7, 2), 10) * 10;
-      return new Date(1970, 0, 1, hours, minutes, seconds, miliseconds);
+  
+
+  function toDate(time) {
+    const hours = parseInt(time.substr(0, 2), 10);
+    const minutes = parseInt(time.substr(2, 2), 10);
+    const seconds = parseInt(time.substr(4, 2), 10);
+    let miliseconds = parseInt(time.substr(7, 2), 10) * 10;
+    if (isNaN(miliseconds)) miliseconds = 0;
+    return new Date(1970, 0, 1, hours, minutes, seconds, miliseconds);
+  }
+
+  function isValidNMEA0183(line) {
+    // Check if the line starts with a '$'
+    if (!(line.startsWith('$') || line.startsWith('!'))) {
+      return false;
     }
 
-    function isValidNMEA0183(line) {
-      // Check if the line starts with a '$'
-      if (!(line.startsWith('$') || line.startsWith('!AIVDM') || line.startsWith('!AIVDO'))) {
-        return false;
-      }
-
-      // Check if the line contains a valid checksum
-      const parts = line.split('*');
-      if (parts.length !== 2) {
-        return false;
-      }
-
-      const message = parts[0].substring(1); // Remove the starting '$'
-      const checksum = parts[1];
-
-      // Calculate the checksum
-      let calculatedChecksum = 0;
-      for (let i = 0; i < message.length; i++) {
-        calculatedChecksum ^= message.charCodeAt(i);
-      }
-
-      // Convert the checksum to a two-digit hexadecimal
-      const hexChecksum = calculatedChecksum.toString(16).toUpperCase().padStart(2, '0');
-
-      // Check if the calculated checksum matches the given checksum
-      return hexChecksum === checksum;
+    // Check if the line contains a valid checksum
+    const parts = line.split('*');
+    if (parts.length !== 2) {
+      return false;
     }
 
-    let previousRMCTime = new Date(1970, 0, 1);
-    let previousSendTime = new Date(1970, 0, 1);
-    app.debug("Plugin started with options: ", options);
-    stopEvent = false;
+    const message = parts[0].substring(1); // Remove the starting '$'
+    const checksum = parts[1];
 
-    // Ensure the filename is given in a full path format
-    const filename = options.filename ? path.resolve(options.filename) : null;
-    const speedFactor = options.speedFactor || 1;
-
-    if (!filename) {
-      app.error("No filename provided");
-      return;
-    } else {
-      startReading();
+    // Calculate the checksum
+    let calculatedChecksum = 0;
+    for (let i = 0; i < message.length; i++) {
+      calculatedChecksum ^= message.charCodeAt(i);
     }
 
+    // Convert the checksum to a two-digit hexadecimal
+    const hexChecksum = calculatedChecksum.toString(16).toUpperCase().padStart(2, '0');
 
-  };
+    // Check if the calculated checksum matches the given checksum
+    return hexChecksum === checksum;
+  }
+
+  let previousRMCTime = new Date(1970, 0, 1);
+  let previousSendTime = new Date(1970, 0, 1);
+
+  app.debug("Plugin started with options: ", options);
+  stopEvent = false;
+
+  // Ensure the filename is given in a full path format
+  const filename = options.filename ? path.resolve(options.filename) : null;
+  const speedFactor = options.speedFactor || 1;
+  if (options.startTime !== "" && options.startTime !== undefined) {
+    startTime = toDate(options.startTime.replace(/:/g, ''));
+  } else {
+    startTime=0;
+  }
+  if (options.endTime !== "" && options.endTime !== undefined) {
+    endTime = toDate(options.endTime.replace(/:/g, ''));
+  } else {
+    endTime=0;
+  }
+  let withinTimeWindow =false;
+
+
+  if (!filename) {
+    app.error("No filename provided");
+    return;
+  } else {
+    startReading();
+  }
+};
 
 
 
+plugin.stop = function () {
+  stopEvent = true;
+  app.debug("Stopping plugin");
+};
 
-  plugin.stop = function () {
-    stopEvent = true;
-    app.debug("Stopping plugin");
-  };
-
-  return plugin;
+return plugin;
 };
